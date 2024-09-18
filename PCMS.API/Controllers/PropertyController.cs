f5ad13c3834c29e27c58fec00c7c9de7c54f6e92
Yousaf Wazir <umbrellaman612@gmail.com>
2024-09-18 17:46:03 +0100

    PropertyController basics

--- PCMS.API/Controllers/PropertyController.cs
+++ PCMS.API/Controllers/PropertyController.cs
@@ -1,6 +1,36 @@
-﻿namespace PCMS.API.Controllers
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PCMS.API.DTOS;
+
+namespace PCMS.API.Controllers
 {
-    public class PropertyController
+    /// <summary>
+    /// Controller for handling property-related actions.
+    /// </summary>
+    /// <remarks>
+    /// Initializes a new instance of the <see cref="PropertyController"/> class.
+    /// </remarks>
+    /// <param name="logger">The logger instance.</param>
+    /// <param name="context">The database context.</param>
+    [ApiController]
+    [Route("properties")]
+    [Authorize]
+    public class PropertyController(ApplicationDbContext context, IMapper mapper) : ControllerBase
     {
+        private readonly ApplicationDbContext _context = context;
+        private readonly IMapper _mapper = mapper;
+
+        /// <summary>
+        /// Creates a new property.
+        /// </summary>
+        /// <param name="request">The DTO containing POST property information.</param>
+        /// <returns>The created property details.</returns>
+        [HttpPost]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<GETProperty>> CreateProperty([FromBody] POSTProperty request)
+        {
+            return Ok();
+        }
     }
 }
