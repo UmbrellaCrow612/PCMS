4a15d679e391b3f1ee77e2110fa91a5c76d9b9d2
Yousaf Wazir <umbrellaman612@gmail.com>
2024-09-18 14:44:30 +0100

    basic DataAnnotations

--- PCMS.API/Models/CrimeScene.cs
+++ PCMS.API/Models/CrimeScene.cs
@@ -1,7 +1,18 @@
-﻿namespace PCMS.API.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace PCMS.API.Models
 {
+    /// <summary>
+    /// Represents a CrimeScene in the system.
+    /// </summary>
+    [Index(nameof(Id), IsUnique = true)]
     public class CrimeScene
     {
+        /// <summary>
+        /// Gets or sets the ID of the CrimeScene, defaults to <see cref="Guid.NewGuid()"/>.
+        /// </summary>
+        [Key]
         public string Id = Guid.NewGuid().ToString();
     }
 }
--- PCMS.API/Models/Department.cs
+++ PCMS.API/Models/Department.cs
@@ -1,7 +1,18 @@
-﻿namespace PCMS.API.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace PCMS.API.Models
 {
+    /// <summary>
+    /// Represents a Department in the system.
+    /// </summary>
+    [Index(nameof(Id), IsUnique = true)]
     public class Department
     {
+        /// <summary>
+        /// Gets or sets the ID of the Department, defaults to <see cref="Guid.NewGuid()"/>.
+        /// </summary>
+        [Key]
         public string Id = Guid.NewGuid().ToString();
     }
 }
