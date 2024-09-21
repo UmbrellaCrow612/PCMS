f4d78da8b984e8d2ab5ea97858ac2e516fb9696d
Yousaf Wazir <umbrellaman612@gmail.com>
2024-09-21 16:11:21 +0100

    tag crud

--- PCMS.API/Dtos/PATCH/PATCHTag.cs
+++ PCMS.API/Dtos/PATCH/PATCHTag.cs
@@ -1,9 +1,17 @@
-﻿namespace PCMS.API.Dtos.PATCH
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PCMS.API.Dtos.PATCH
 {
     /// <summary>
     /// PATCH a Tag
     /// </summary>
     public class PATCHTag
     {
+        [Required]
+        [MaxLength(100)]
+        public required string Name { get; set; }
+
+        [MaxLength(500)]
+        public string? Description { get; set; }
     }
 }
